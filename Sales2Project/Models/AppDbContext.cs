7fa80f72239689581ad1e1e8271a2c2802f58e33
myla steiger <mrs.steiger@gmail.com>
2022-02-17 20:01:22 -0500

    Add project files.

---
+++ Sales2Project/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales2Project.Models {
+    public class AppDbContext : DbContext {
+        public virtual DbSet<Customer> Customers { get; set;}
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder Builder);
+        }
+    }
+
---
+++ Sales2Project/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales2Project.Models {
+    public class Customer {
+        public int Id { get; set; }
+        [Required, StringLength(80)]
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        [Required, StringLength(2)]
+        public string StateCode { get; set; }
+        [Column(TypeName = "decimal(7,2)")]
+        public decimal Sales { get; set; }
+
+        public Customer() {
+        }
+    }
+}
